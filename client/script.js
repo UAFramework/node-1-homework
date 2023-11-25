a6e805c93a2f6f61c90413b86719387ab07c497e
Pavel <pavel.filkovskiy@gmail.com>
2023-11-25 12:45:28 +0000

    make corrections

--- client/script.js
+++ client/script.js
@@ -1,77 +1,107 @@
-window.onload = function () {
-  fetch("http://localhost:3000/todos")
-  .then(response => response.json())
-  .then(todos => {
-    todos.map(item => {
-      addTodo(item);
-    });
-  })
-}
-
 function addTodo(item) {
-    const todoItem = document.createElement('li');
-    todoItem.setAttribute('id', item.id);
-    todoItem.innerHTML = `
-      ${item.title}
-      <span>
-        <button class="completeButton">&#10003;</button>
-        <button class="modifyButton">&#9998;</button>
-        <button class="deleteButton">&#128465;</button>
-      </span>
-    `;
-    if (item.done) {
-      todoItem.classList.add('completed');
-    }
-    document.getElementById('todoList').appendChild(todoItem);
+  const todoList = document.getElementById('todoList');
+  
+  const todoItem = document.createElement('li');
+  todoItem.setAttribute('id', item.id);
+  todoItem.innerHTML = 
+    `${item.title}
+    <span>
+      <button class="completeButton" taskid="${item.id}">&#10003;</button>
+      <button class="modifyButton" taskid="${item.id}">&#9998;</button>
+      <button class="deleteButton" taskid="${item.id}">&#128465;</button>
+    </span>`;
 
-    const buttons = todoItem.querySelectorAll('button');
-    buttons.forEach(button => {
-      if (button.classList.contains('completeButton')) {
-        button.addEventListener('click', function (event) {
-          const parentLiId = event.target.parentNode.parentNode.id;
-          fetch(`http://localhost:3000/todos`, {
-          method: "PATCH", 
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-              id: +parentLiId})
-          }).then(res => {
-              completeTodo(event.target);
-              console.log(res.status);
-          })
-        });
-      } else if (button.classList.contains('modifyButton')) {
-        button.addEventListener('click', modifyTodo);
-      } else if (button.classList.contains('deleteButton')) {
-        button.addEventListener('click', function (event) {
-          const parentLiId = event.target.parentNode.parentNode.id;
-          fetch(`http://localhost:3000/todos/:${parentLiId}`, {
-          method: "DELETE", 
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-              id: +parentLiId})
-          }).then(res => {
-              deleteTodo(event.target);
-              const todosContainer = document.querySelector('ul');
-              const todosList = todosContainer.querySelectorAll('li');
-              updateIdForLi(todosList);
-              console.log(res.status);
-          })
-        });
-      }
-    });
-}
-
-function completeTodo(element) {
-    const todoItem = element.parentNode.parentNode;
+  /**
+   * TODO: 
+   * refactor structure of the item.
+   * a better structure would be:
+   * 
+   * <div class="task-container">
+   *  <div class="task-title">....</div>
+   *  <div class="task-buttons">
+   *    <button class="completeButton" />
+   *    <button class="modifyButton" />
+   *    <button class="deleteButton" />
+   *  </div>
+   * </div>
+   */ 
+  
+  if (item.done) {
     todoItem.classList.add('completed');
+  }
+
+  todoList.appendChild(todoItem);
+
+  /**
+   * TODO: 
+   * this is still not the best way to do it.
+   * we can assign event listener to the button above, while creating it separately
+   */
+  const completeButtons = todoItem.querySelectorAll('button.completeButton');
+  completeButtons.forEach(button => {
+    button.addEventListener('click', function (event) {
+      const taskid = event.target.getAttribute("taskid");
+      
+      fetch(`http://localhost:3000/task/${taskid}`, {
+        method: "PUT", 
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({done: true})
+      }).then(res => {
+          if (res.ok) {
+            toggleCompleteTodo(taskid);
+          }
+      }).catch(error => {
+          // do something with the error.
+      });
+    });
+  });
+
+  const modifyButtons = todoItem.querySelectorAll('button.modifyButton');
+  modifyButtons.forEach(button => {
+    button.addEventListener('click', modifyTodo);
+  });
+
+  const deleteButtons = todoItem.querySelectorAll('button.deleteButton');
+  deleteButtons.forEach(button => {
+    button.addEventListener('click', function (event) {
+      let taskid = event.target.getAttribute("taskid");
+      fetch(`http://localhost:3000/task/${taskid}`, {
+        method: "DELETE"
+      }).then(res => {
+          if (res.ok) {
+            deleteTodo(taskid);
+          }
+      })
+      .catch(error => {
+        // todo...
+      });
+    });
+  });
 }
 
+function toggleCompleteTodo(id) {
+    const taskElement = document.querySelector(`li[id='${id}']`);
+    if (taskElement.classList.contains("completed")) {
+      taskElement.classList.remove('completed');
+    } else {
+      taskElement.classList.add('completed');
+    }
+    
+}
+
+function deleteTodo(id) {
+  const taskElement = document.querySelector(`li[id='${id}']`);
+  taskElement.remove();
+}
+
+
+// TODO: refactor below:
 function modifyTodo(event) {
   const button = event.target;
+  let taskid = button.getAttribute("taskid");
+  
   const todoItem = button.parentNode.parentNode;
   const todoTitle = todoItem.firstChild.textContent.trim();
 
@@ -107,62 +137,93 @@ function modifyTodo(event) {
   todoItem.appendChild(saveButton);
 }
 
-function deleteTodo(element) {
-    const todoItem = element.parentNode.parentNode;
-    todoItem.parentNode.removeChild(todoItem);
-}
-
 function removeCompletedTodos() {
   const completedTodos = document.querySelectorAll('.completed');
   completedTodos.forEach(todo => todo.remove());
 }
 
-function updateIdForLi (arr) {
-  arr.forEach((item, index) => {
-    item.id = index + 1; 
+
+function addEventListenersInNewTodoContainer() {
+    document.querySelector('#newTodoContainer button:nth-of-type(1)').addEventListener('click', function () {
+      fetch(`http://localhost:3000/task/${taskid}`, {
+          method: "POST", 
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+              title: document.getElementById('newTodoInput').value,
+              done: false})
+      }).then(res => {
+        const todoItem = document.getElementById('newTodoInput');
+        const todosContainer = document.querySelector("ul");
+        const liCount = todosContainer.querySelectorAll('li').length;
+        if (todoItem.value.trim() !== '') {
+          let item = {
+            title: todoItem.value.trim(),
+            done: false,
+            id: liCount + 1
+          };
+          addTodo(item);
+          document.getElementById('newTodoInput').value = '';
+          console.log(res);
+        } else {
+          alert('Please enter a todo title!');
+        }
+      })
+      
+  });
+
+  document.querySelector('#newTodoContainer button:nth-of-type(2)').addEventListener('click', function () {
+    fetch("http://localhost:3000/todos", {
+          method: "PUT", 
+          headers: {
+              "Content-Type": "application/json",
+          }
+      }).then(res => {
+          removeCompletedTodos();
+          const todosContainer = document.querySelector('ul'); // Get the <ul> element
+          const todosList = todosContainer.querySelectorAll('li');
+          console.log(res);
+      })
+  });
+};
+// ---  end of section that needs to be refactored.
+
+function buildToDoList() {
+  fetch("http://localhost:3000/task")
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } 
+    
+    // instead of throwing error 
+    // we might want to shoe a popup overlay on our page.
+    // and make it look nice and cool
+    // some examples here: 
+    // https://www.w3schools.com/howto/howto_css_overlay.asp
+    throw new Error("Resonse was not Ok!");
+
+  })
+  .then(todos => {
+    todos.forEach(item => {
+      addTodo(item);
+    });
+  })
+  .catch(error => {
+    // this error happens when fetch itself totally failed.
+    // in case server is completely down, or there is no internet connection.
+    // in this case we still might want to let user know out the problem.
+    // if we don't catch it, the rest of the functinality on page won't work.
+    // script processing crashes our script.js will stop running at the client's side.
+    let errorMessage = `AUCHTUNG!!!\n\nRequest to todo server has failed with error:\n${error.message}`;
+    alert(errorMessage);
   });
 }
 
-document.querySelector('#newTodoContainer button:nth-of-type(1)').addEventListener('click', function () {
-    fetch("http://localhost:3000/todos", {
-        method: "POST", 
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            title: document.getElementById('newTodoInput').value,
-            done: false})
-    }).then(res => {
-      const todoItem = document.getElementById('newTodoInput');
-      const todosContainer = document.querySelector("ul");
-      const liCount = todosContainer.querySelectorAll('li').length;
-      if (todoItem.value.trim() !== '') {
-        let item = {
-          title: todoItem.value.trim(),
-          done: false,
-          id: liCount + 1
-        };
-        addTodo(item);
-        document.getElementById('newTodoInput').value = '';
-        console.log(res);
-      } else {
-        alert('Please enter a todo title!');
-      }
-    })
-    
-});
 
-document.querySelector('#newTodoContainer button:nth-of-type(2)').addEventListener('click', function () {
-  fetch("http://localhost:3000/todos", {
-        method: "PUT", 
-        headers: {
-            "Content-Type": "application/json",
-        }
-    }).then(res => {
-        removeCompletedTodos();
-        const todosContainer = document.querySelector('ul'); // Get the <ul> element
-        const todosList = todosContainer.querySelectorAll('li');
-        updateIdForLi(todosList);
-        console.log(res);
-    })
-});
+function setup () {
+  buildToDoList();
+  addEventListenersInNewTodoContainer();
+}
+
+window.onload = setup();
